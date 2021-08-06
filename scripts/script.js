e242a66762ac43b741a4234cf3b3e6b27e5708b1
Jordan Tejeda <tejedajordan@gmail.com>
2021-08-06 01:09:12 -0400

    saved new fitness website in git incase of accidental deletion

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Jordan Tejeda
+    File Name: script.js
+    Date: June 04, 2021
+*/
+//jQuery for hero image to consume the header window space
+
+$(document).ready(function(){
+	$('.hero').height($(window).height());
+	});
